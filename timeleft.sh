11ce1b346b712a5b1bea172b4cf27cba59e42e4d
Gabriel <gnunezyuve@gmail.com>
2019-01-18 14:01:00 -0300

    Aprolijando el formato de la hora

--- timeleft.sh
+++ timeleft.sh
@@ -3,7 +3,6 @@ HOURS=7
 MINUTES=15
 
 TOTAL=$(( $(($HOURS*60)) + $MINUTES )) 
-# CHECK_IN=$(cat /home/gabrieln/scripts/actualDate)
 CHECK_IN=$(cat $HOME/.config/actualDate)
 
 EPOCH_LEFT=$(( `date -d "$CHECK_IN" +%s` + $(( $TOTAL * 60 )) - `date +%s` ))
@@ -13,4 +12,4 @@ SECONDS_LEFT=$(( EPOCH_LEFT % 60 ))
 HOURS_LEFT=$(( $EPOCH_LEFT / 60 / 60 ))
 MINUTES_LEFT=$(( EPOCH_LEFT / 60  - HOURS_LEFT *60 ))
 
-echo $HOURS_LEFT:$MINUTES_LEFT:$SECONDS_LEFT
+date -d $HOURS_LEFT:$MINUTES_LEFT:$SECONDS_LEFT +"%_H:%M:%S"
