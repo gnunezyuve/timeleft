61a231fcff66bacdc6b8cfc179dfcb1421dc46a7
Gabriel <gnunezyuve@gmail.com>
2019-03-08 10:42:28 -0300

    Wea para que me tire una push con la hora de salida cuando clickeo el icono

--- timeleft.sh
+++ timeleft.sh
@@ -11,6 +11,10 @@ SECONDS_LEFT=$(( EPOCH_LEFT % 60 ))
 HOURS_LEFT=$(( $EPOCH_LEFT / 60 / 60 ))
 MINUTES_LEFT=$(( EPOCH_LEFT / 60  - HOURS_LEFT *60 ))
 
+if [ $BLOCK_BUTTON -eq 3 ]; then
+  notify-send "Departure: `$HOME/bin/takeoff`"
+fi
+
 if [ "$EPOCH_LEFT" -gt 0 ]; then
   date -d $HOURS_LEFT:$MINUTES_LEFT:$SECONDS_LEFT +"%_H:%M"
 fi
